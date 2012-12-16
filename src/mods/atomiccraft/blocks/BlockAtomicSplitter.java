b1f9892070904dfb4885afbe7e0c5a669d4ab1c4
TrainerGuy22 <rocketkid1@gmail.com>
2012-12-16 11:42:29 -0500

    Side facing player on place, code by Rama.

--- src/mods/atomiccraft/blocks/BlockAtomicSplitter.java
+++ src/mods/atomiccraft/blocks/BlockAtomicSplitter.java
@@ -15,5 +15,49 @@ public class BlockAtomicSplitter extends Block  {
 	public String getTextureFile(){
 		return "/atomiccraft/blocks.png";
 	}
+	
+	@Override
+	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLiving placer){
+		float pitch = placer.rotationPitch;
+		float yaw = placer.rotationYaw;
+		
+		int side;
+		
+		if (pitch > 45.0f)
+		{
+			side = ForgeDirection.UP.ordinal();
+		}
+		else if (pitch < -45.0f)
+		{
+			side = ForgeDirection.DOWN.ordinal();
+		}	
+		else
+		{
+			side = MathHelper.floor_float(0.5f+yaw/90.0f) & 3;
+				
+			switch (side)
+			{
+			case 0:
+				side = ForgeDirection.NORTH.ordinal();
+				break;
+			case 1:
+				side = ForgeDirection.EAST.ordinal();
+				break;
+			case 2:
+				side = ForgeDirection.SOUTH.ordinal();
+				break;
+			case 3:
+				side = ForgeDirection.WEST.ordinal();
+				break;
+			}
+		}
+				
+		world.setBlockMetadata(x, y, z, side);
+	}
+	
+	@Override
+	public ForgeDirection getOrientation(World world, int x, int y, int z){
+		return ForgeDirection.getOrientation(world.getBlockMetadata(x, y, z));
+	}
 
 }
